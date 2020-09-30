8aea5184a1f3f3550a10c3cfe3327d831fae1f04
Saad Zridi <saadzridi25@gmail.com>
2020-10-01 01:56:27 +0200

    First Commit : Begin Project

---
+++ app/src/main/java/com/example/contactapp/ContactDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.contactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_display);
+    }
+}
